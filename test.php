d7dde7de02df30d22557c79fbeba55efb0d55518
Kyle Vermeer <kvermeer@stanford.edu>
2012-10-23 23:46:54 -0700

    Added some starter files

---
+++ test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Test Page</title>
+<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+    <?php
+    echo "<h1> Hello! </h1>"
+    ?>
+</body>
+</html>
