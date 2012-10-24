d8f538a903cbc943ba9d089e3d862e96598c0b08
Kyle Vermeer <kvermeer@stanford.edu>
2012-10-24 14:09:57 -0700

    Working on database stuff

--- login.php
+++ login.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
 <?php
-    $title = "Login Here!";
-    include 'meta.php' //Always include this file, has many necessary, but redundant files
-?>
-</head>
-<body>
-    <?php
-    include 'header.php';
-    ?>
+    include 'config.php';
     
-</body>
-</html>
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    echo '<p>'.$username.'</p>';
+    echo '<p>'.$password.'</p>';
+?>
