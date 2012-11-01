89c664199f3d2fd37153ea5cd1bf7ee892b20d21
Kyle Vermeer <kvermeer@stanford.edu>
2012-11-01 06:24:08 -0700

    Added new icon and load screen

--- utils/meta.php
+++ utils/meta.php
@@ -8,6 +8,9 @@
 <!-- Mobile Shit -->
 <meta name="apple-mobile-web-app-capable" content="yes" />  
 <meta name="apple-mobile-web-app-status-bar-style" content="default" />  
+<link href="../static/media/WhatsUpLoadingbig.jpg" rel="apple-touch-startup-image" media="(device-height: 568px)">
+<link rel="apple-touch-startup-image" href="../static/media/WhatsUpLoading.jpg"/>
+<link rel="apple-touch-icon" href="../static/media/apple-touch-icon-iphone.png" />
 <meta name="viewport" content = "width = device-width, initial-scale = 1" />  
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
 <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
