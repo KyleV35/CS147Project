d8972ef19718cb09b437db41009653c36eaf75cf
Kyle Vermeer <kvermeer@stanford.edu>
2012-11-07 15:15:42 -0800

    Added iScroll

--- utils/meta.php
+++ utils/meta.php
@@ -19,6 +19,7 @@
 <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
 <script src="../static/javascripts/localStorage.js"></script>
+<script src="../static/javascripts/iscroll.js"></script>
 <script>
 $.ajaxSetup ({
 // Disable caching of AJAX responses
